fd1907114c808a94c0ec37f5057f9c5208874f22
sgrichards <steve.richards@acquia.com>
2023-03-14 10:31:46 +0000

    testing failure

--- drupal/scripts/install.sh
+++ drupal/scripts/install.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-set -uo pipefail
+set -eo pipefail
 
 composer install --no-interaction
 
 if [ ! -f "/opt/drupal/web/sites/default/settings.php" ]; then
     echo "Installing Drupal..."
-    drush --root=/opt/drupal/web si cohesion_install --db-url=mysql://root:${MARIADB_ROOT_PASSWORD}@database:3306/${MYSQL_DATABASE} --account-name=${DRUPAL_USER} --account-pass=${DRUPAL_PWD} -y -v
+    drush --root=/opt/drupal/web si cohesion_install --db-url=mysql://root:MARIADB_ROOT_PASSWORD@database:3306/${MYSQL_DATABASE} --account-name=${DRUPAL_USER} --account-pass=${DRUPAL_PWD} -y -v
 
     drush --root=/opt/drupal/web en sitestudio_uikit --debug -y -v
 
