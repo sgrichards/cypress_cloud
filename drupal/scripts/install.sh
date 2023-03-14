33a2872ff60ba59ee60578c7e68c1a681e44e90d
sgrichards <steve.richards@acquia.com>
2023-03-14 10:47:53 +0000

    returns error

--- drupal/scripts/install.sh
+++ drupal/scripts/install.sh
@@ -5,9 +5,14 @@ set -euo pipefail
 composer install --no-interaction
 
 if [ ! -f "/opt/drupal/web/sites/default/settings.php" ]; then
-    echo "Installing Drupal..."
-    drush --root=/opt/drupal/web si cohesion_install --db-url=mysql://root:MARIADB_ROOT_PASSWORD@database:3306/${MYSQL_DATABASE} --account-name=${DRUPAL_USER} --account-pass=${DRUPAL_PWD} -y -v
 
+    echo "Installing Drupal...\n"
+    if drush --root=/opt/drupal/web si cohesion_install --db-url=mysql://root:MARIADB_ROOT_PASSWORD@database:3306/${MYSQL_DATABASE} --account-name=${DRUPAL_USER} --account-pass=${DRUPAL_PWD} -y -v; then
+      echo "Success - site installed\n"
+    else
+      echo "Install failed\n"
+      exit 1
+    fi
     drush --root=/opt/drupal/web en sitestudio_uikit --debug -y -v
 
     chown -R www-data:www-data /opt/drupal/web/sites/default
