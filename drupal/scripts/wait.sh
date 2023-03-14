a9ff7e029405bc98a8785cb673a18aab8a3bd28e
sgrichards <steve.richards@acquia.com>
2023-03-14 11:44:15 +0000

    fix up - look sharp

--- drupal/scripts/wait.sh
+++ drupal/scripts/wait.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-set -uo pipefail
+set -euo pipefail
 
 if [ ! -f "/app/drupal/docroot/sites/default/settings.php" ]; then
-    while ! mysqladmin -h database -u${MYSQL_USER} -p${MYSQL_PASSWORD} ping --silent; do
+    while ! mysqladmin -h database -uroot -p${MARIADB_ROOT_PASSWORD} ping --silent; do
         echo "Waiting for db host..."
         sleep 1
     done
